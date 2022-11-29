57d9349e38d58d442d5cb68b337df82a5dc92f3c
Lokesh Surana <lokesh8203@gmail.com>
2022-11-29 14:38:52 +0530

    Added code for reading from CSV

--- directory.cpp
+++ directory.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main ()
 {
     
-    return 0;
+    ifstream fin("BM2043_PROJECT_DATA.csv");
+    string line;
+    getline(fin, line); // skip the first line from CSV
+    while(fin.good()){
+        getline(fin, line, ',');
+        cout << line << endl;
+    }
+    return 0; 
 }
